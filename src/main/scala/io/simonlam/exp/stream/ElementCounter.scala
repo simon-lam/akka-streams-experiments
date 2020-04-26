0adfc95b6f564ee62283a093e5ad20058748ef69
Simon Lam <simonlam6@gmail.com>
2020-04-26 10:34:23 -0500

    Materialized element counter probe

--- src/main/scala/io/simonlam/exp/stream/ElementCounter.scala
+++ src/main/scala/io/simonlam/exp/stream/ElementCounter.scala
@@ -1,23 +1,56 @@
 package io.simonlam.exp.stream
 
-import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
-import akka.stream.stage.{GraphStage, GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicLong}
 
-class ElementCounter[T] extends GraphStage[FlowShape[T, T]] {
+import akka.stream.stage.{GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
+import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
+
+class ElementCounter[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], ElementCounterProbe] {
 
   val in = Inlet[T]("ElementCounter.in")
   val out = Outlet[T]("ElementCounter.out")
 
-  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
-    new GraphStageLogic(shape) with InHandler with OutHandler {
-      override def onPush(): Unit = push(out, grab(in))
+  override def shape: FlowShape[T, T] = FlowShape(in, out)
+
+  override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, ElementCounterProbe) = {
+    val elementCount = new AtomicLong(0L)
+    val isStreamActive = new AtomicBoolean(true)
+
+    val logic = new GraphStageLogic(shape) with InHandler with OutHandler {
+      override def onPush(): Unit = push(out, {
+        elementCount.incrementAndGet()
+        grab(in)
+      })
 
       override def onPull(): Unit = pull(in)
+
+      override def onUpstreamFinish(): Unit = {
+        isStreamActive.set(false)
+        super.onUpstreamFinish()
+      }
+
+      override def onUpstreamFailure(ex: Throwable): Unit = {
+        isStreamActive.set(false)
+        super.onUpstreamFailure(ex)
+      }
+
+      override def onDownstreamFinish(cause: Throwable): Unit = {
+        isStreamActive.set(false)
+        super.onDownstreamFinish(cause)
+      }
 
       setHandlers(in, out, this)
     }
 
-  override def shape: FlowShape[T, T] = FlowShape(in, out)
+    (logic, new ElementCounterProbe {
+      override def count: Long = elementCount.get()
+
+      override def isStreamFinished: Boolean = !isStreamActive.get()
+    })
+  }
 }
 
-
+trait ElementCounterProbe {
+  def count: Long
+  def isStreamFinished: Boolean
+}
--- src/test/scala/io/simonlam/exp/stream/ElementCounterSpec.scala
+++ src/test/scala/io/simonlam/exp/stream/ElementCounterSpec.scala
@@ -3,7 +3,8 @@ package io.simonlam.exp.stream
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Flow, Source}
+import akka.stream.scaladsl.{Flow, Keep, Source}
+import akka.stream.testkit.scaladsl.TestSink
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
 
@@ -19,5 +20,25 @@ class ElementCounterSpec extends AnyFlatSpec with Matchers with ScalaFutures wit
       .via(Flow.fromGraph(new ElementCounter))
       .runFold(List.empty[Int])((agg, el) => agg ::: List(el)).futureValue
     result shouldBe intList
+  }
+
+  it should "report seen elements from outside the stream" in {
+    val (counterProbe, testProbe) = Source((1 to 5).toList)
+      .viaMat(Flow.fromGraph(new ElementCounter))(Keep.right)
+      .toMat(TestSink.probe[Int])(Keep.both)
+      .run()
+
+    counterProbe.count shouldBe 0L
+
+    testProbe.request(1).expectNext(1)
+    counterProbe.count shouldBe 1L
+
+    testProbe.request(3).expectNext(2, 3, 4)
+    counterProbe.count shouldBe 4L
+    counterProbe.isStreamFinished shouldBe false
+
+    testProbe.request(3).expectNext(5).expectComplete()
+    counterProbe.count shouldBe 5L
+    counterProbe.isStreamFinished shouldBe true
   }
 }
