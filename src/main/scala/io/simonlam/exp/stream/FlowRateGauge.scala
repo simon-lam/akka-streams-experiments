46be092e5b54a3bbd8fa26db5e3cb6d48b553826
Simon Lam <simonlam6@gmail.com>
2020-04-26 16:46:16 -0500

    Support the ability to take readings from the gauge over arbitrary periods of time

--- src/main/scala/io/simonlam/exp/stream/FlowRateGauge.scala
+++ src/main/scala/io/simonlam/exp/stream/FlowRateGauge.scala
@@ -1,13 +1,15 @@
 package io.simonlam.exp.stream
 
 import java.time.ZonedDateTime
-import java.util.concurrent.atomic.{AtomicBoolean, AtomicLong}
+import java.util.UUID
+import java.util.concurrent.atomic.AtomicLong
 
 import akka.stream.stage._
 import akka.stream.{Attributes, FlowShape, Inlet, Outlet}
 
 import scala.collection.mutable
 import scala.concurrent.duration._
+import scala.concurrent.{Future, Promise}
 
 class FlowRateGauge[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], FlowRateGaugeProbe] {
 
@@ -20,7 +22,9 @@ class FlowRateGauge[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], 
     val logic = new FlowRateGaugeGraphStageLogic[T](shape)
 
     (logic, new FlowRateGaugeProbe {
-      override def takeReading: BigDecimal = logic.flowRate.asElementsPerSecond
+      override def takeReading: BigDecimal = logic.defaultReading.asElementsPerSecond
+
+      override def takeReading(duration: FiniteDuration): Future[BigDecimal] = logic.takeReading(duration)
     })
   }
 }
@@ -28,51 +32,69 @@ class FlowRateGauge[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], 
 class FlowRateGaugeGraphStageLogic[T](shape: FlowShape[T, T], interval: FiniteDuration = 1 seconds)
   extends TimerGraphStageLogic(shape) with InHandler with OutHandler {
 
-  private final val DefaultSamplingTimer = "default-sample"
+  private final val DefaultReadingTimer = "default-sample"
 
   private val in = shape.in
   private val out = shape.out
 
-  private val elementsProcessed = new AtomicLong(0L)
+  private val elementsSeen = new AtomicLong(0L)
 
   override def onPush(): Unit = push(out, {
-    initDefaultSamplingTimer()
-    elementsProcessed.incrementAndGet()
+    initDefaultReadingTimer()
+    elementsSeen.incrementAndGet()
     grab(in)
   })
 
   override def onPull(): Unit = pull(in)
 
-  private val samples = mutable.Map.empty[String, FlowRateGaugeSample]
+  setHandlers(in, out, this)
 
-  private def initDefaultSamplingTimer(): Unit = {
-    if (!isTimerActive(DefaultSamplingTimer)) {
-      scheduleAtFixedRate(DefaultSamplingTimer, 0 seconds, interval)
+  private val readings = mutable.Map.empty[String, FlowRateGaugeReading]
+  private val readingsInProgress = mutable.Map.empty[String, Promise[FlowRateGaugeReading]]
+
+  def defaultReading = {
+    val currentElementCount = elementsSeen.get()
+    readings.getOrElseUpdate(DefaultReadingTimer,
+      FlowRateGaugeReading(currentElementCount, currentElementCount, interval))
+  }
+
+  def takeReading(duration: FiniteDuration): Future[FlowRateGaugeReading] = {
+    val readingKey = UUID.randomUUID().toString
+    val currentElementCount = elementsSeen.get()
+    readings.update(readingKey, FlowRateGaugeReading(currentElementCount, currentElementCount, duration))
+    val promise = Promise[FlowRateGaugeReading]()
+    readingsInProgress.update(readingKey, promise)
+    promise.future
+  }
+
+  private def initDefaultReadingTimer(): Unit = {
+    if (!isTimerActive(DefaultReadingTimer)) {
+      scheduleAtFixedRate(DefaultReadingTimer, 0 seconds, interval)
     }
   }
 
   override def onTimer(timerKey: Any): Unit = {
     timerKey match {
-      case sampleKey: String =>
-        val currentElementCount = elementsProcessed.get()
-        val sample = flowRate
-        samples.update(sampleKey, sample.update(currentElementCount))
+      case r: String if r == DefaultReadingTimer =>
+        val currentElementCount = elementsSeen.get()
+        val sample = defaultReading
+        readings.update(r, sample.update(currentElementCount))
+
+      case r: String =>
+        val currentElementCount = elementsSeen.get()
+        val reading = readings.get(r).get // TODO
+        val promise = readingsInProgress.get(r).get // TODO
+        promise.success(reading.update(currentElementCount))
+        readingsInProgress.remove(r)
+        readings.remove(r)
     }
   }
-
-  def flowRate = {
-    val currentElementCount = elementsProcessed.get()
-    samples.getOrElseUpdate(DefaultSamplingTimer,
-      FlowRateGaugeSample(currentElementCount, currentElementCount, interval))
-  }
-
-  setHandlers(in, out, this)
 }
 
-case class FlowRateGaugeSample(startingElementCount: Long,
-                               endingElementCount: Long,
-                               duration: Duration,
-                               timestamp: ZonedDateTime = ZonedDateTime.now()) {
+case class FlowRateGaugeReading(startingElementCount: Long,
+                                endingElementCount: Long,
+                                duration: FiniteDuration,
+                                timestamp: ZonedDateTime = ZonedDateTime.now()) {
   def update(recordedElementCount: Long) = this.copy(
     startingElementCount = endingElementCount,
     endingElementCount = recordedElementCount,
@@ -86,4 +108,6 @@ case class FlowRateGaugeSample(startingElementCount: Long,
 trait FlowRateGaugeProbe {
   // Elements per second
   def takeReading: BigDecimal
+
+  def takeReading(duration: FiniteDuration): Future[BigDecimal]
 }
